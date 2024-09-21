667fdb18a98d9ed15019b9c4b538b542b2ba93fc
Cherrypick14 <cherrypicklet@gmail.com>
2024-09-22 01:07:21 +0200

    Add the testimonial info content

--- src/kintsugi_frontend/src/components/Storiessection.jsx
+++ src/kintsugi_frontend/src/components/Storiessection.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import Slider from 'react-slick';
 import '../styles/styles.css';
 import 'slick-carousel/slick/slick.css';
+import '../styles/stories.css';
 import 'slick-carousel/slick/slick-theme.css';
 
 const StoriesSection = () => {
