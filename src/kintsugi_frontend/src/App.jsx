25d77d3e1be4b7c8ca726a4e4d28fad743a27959
Cherrypick14 <cherrypicklet@gmail.com>
2024-09-28 22:32:11 +0300

    Add groupmanager component

--- src/kintsugi_frontend/src/App.jsx
+++ src/kintsugi_frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Login from './pages/Login';
 import AdminDashboard from './pages/AdminDashboard';
 import Dao from './pages/Dao';
 import DAODashboard from './pages/Dao';
+import GroupManager from './components/Groupmanager';
 
 
 function App() {
@@ -18,6 +19,7 @@ function App() {
         <Route path='/home' element={<Home />} />
         <Route path="/statistics" element={<Statistics />} />
         <Route path='/form' element={<Form />}/>
+        <Route path = '/groupmanager' element ={<GroupManager />} />
         <Route path='/login' element={<Login />}/>
         <Route path="/admin" element={<AdminDashboard />} />
         <Route path="/dao/:id" element={<DAODashboard />} /> 
